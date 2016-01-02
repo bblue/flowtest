9a8ec1ead23683cabed3450f962c965989b873af
bblue <aleksander.lanes@gmail.com>
2016-01-02 01:39:29 +0100

    adding guest provider

---
+++ src/Package/RecognitionPackage/GuestProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace bblue\ruby\Package\RecognitionPackage;
+
+use bblue\ruby\Component\Core\iUserProvider;
+use bblue\ruby\Entities\Guest;
+
+final class GuestProvider implements iUserProvider
+{
+	public function getById($id)
+	{
+		if($id === Guest::GUEST_ID) {
+			return new Guest;
+		}
+	}
+
+	public function getByUsername($username)
+	{
+		if($username === Guest::GUEST_USERNAME) {
+			return new Guest;
+		}
+	}
+}
