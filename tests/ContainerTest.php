b63ed0066d5379eb0eed534cc51333d578d1feec
bblue <aleksander.lanes@gmail.com>
2016-01-28 12:10:53 +0100

    Initial test case for container

---
+++ tests/ContainerTest.php
@@ -0,0 +1,28 @@
+<?php
+use bblue\ruby\Component\Container\Container;
+
+/**
+ * Created by PhpStorm.
+ * User: Aleksander Lanes
+ * Date: 28.01.2016
+ * Time: 11:50
+ */
+class ContainerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Container
+     */
+    public $container;
+
+    public function __construct()
+    {
+        $this->container = $this->getMockBuilder('\bblue\ruby\Component\Container\Container')
+    }
+
+    public function testRegisterClassWithoutAlias()
+    {
+        $class = new stdClass();
+        $this->container->register($class);
+        $this->assertTrue($this->container->has('stdClass'));
+    }
+}
